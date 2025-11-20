6fb9de42e407cf414854f351b6826b8ba75fca88
Yan <70453458+FarwayDot@users.noreply.github.com>
2025-11-20 18:30:53 -0500

    Avance

--- Simulation/Matlab/Hyst.m
+++ Simulation/Matlab/Hyst.m
@@ -4,30 +4,58 @@ clc
 
 %% Initial variables
 
-Vcc = 3.0;
-Vpu = 3.0;
-Vref = 3.0;
+Vcc = 3.3;
+Vref = 3.3;
 
-Vh = 2.5;
-Vl = 2.2;
+Vh = 0;
+Vl = 0;
 Vhyst = Vh-Vl;
 
-R1 = 0;
-R2 = 500e3;
+R1 = 100e3;
+R2 = 30e3;
 R3 = 0;
-R4 = 0;
+R4 = 100e3;
 
-A = (Vref/Vl)-1;
-B = Vref - Vh;
-C = Vh/R2;
-D = Vpu- Vh;
-
-first_pol = (B/A) - (C*R2);
-second_pol = (B/A)*(R2+R3) - (C*R2*R3) + (D*R2);
-third_pol = (B/A)*R2*R3;
-
-pol = [first_pol second_pol third_pol];
-r = roots(pol)
+Vh = Vref/(1+(R1/R2)+(R1/R4))
+Vl = (Vref/R1 + Vcc/R4)/((1/R1) + (1/R2) + (1/R4))
 
 
+% R1_H = (Vh*(R2+R4))/((Vref-Vh)*R2*R4)
+% R1_L = (Vref-Vl)/((Vl/R2)+((Vl-Vcc)/R4))
+% 
+% 
+% A = (Vref-Vl)*R2*R2;
+% B = Vh*Vl/(Vref-Vh);
+% 
+% C = Vh*(Vcc-2*Vl)*R2;
+% D = Vref-Vh;
+% 
+% E = Vh*(Vl-Vcc)*R2*R2;
+% F = Vref - Vh;
+% 
+% first_pol = A-B;
+% second_pol = C/D;
+% third_pol = E/F;
+% 
+% pol = [first_pol second_pol third_pol];
+% r = roots(pol)
+
+
+% R1 = (Vref - Vh)/((Vh/R2)-((Vpu-Vh)/(R3+R4)))
+% R1_1 = (Vref-Vl)/(Vl*((R2+R4)/(R2*R4)))
+% 
+% A = (Vref/Vl)-1;
+% B = Vref - Vh;
+% C = Vh/R2;
+% D = Vpu- Vh;
+% 
+% first_pol = (B/A) - (C*R2);
+% second_pol = (B/A)*(R2+R3) - (C*R2*R3) + (D*R2);
+% third_pol = (B/A)*R2*R3;
+% 
+% pol = [first_pol second_pol third_pol];
+% r = roots(pol)
+
+
+%% R1
 
